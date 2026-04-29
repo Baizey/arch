52baa322a67b5caad75c17b65cf28f78ee14b838
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2026-04-29 18:39:12 +0200

    cleanup

--- agent/src/main/kotlin/org/baizey/harness/HarnessContext.kt
+++ agent/src/main/kotlin/org/baizey/harness/HarnessContext.kt
@@ -2,19 +2,16 @@ package org.baizey.harness
 
 import org.baizey.harness.policy.PathPolicyLogic
 import org.baizey.harness.policy.GitPolicyLogic
-import org.baizey.harness.policy.ShellPolicyLogic
 
 class HarnessContext(
     val interactionPort: HarnessInteractionPort
 ) {
     val pathPolicyLogic: PathPolicyLogic = PathPolicyLogic(interactionPort)
-    val shellPolicyLogic: ShellPolicyLogic = ShellPolicyLogic(interactionPort)
     val gitPolicyLogic: GitPolicyLogic = GitPolicyLogic(interactionPort)
 
 
     fun reloadFromPersistence() {
         pathPolicyLogic.reloadFromPersistence()
-        shellPolicyLogic.reloadFromPersistence()
         gitPolicyLogic.reloadFromPersistence()
     }
 }
--- agent/src/main/kotlin/org/baizey/harness/policy/shell/ShellPersistedPolicy.kt
+++
@@ -1,8 +0,0 @@
-package org.baizey.harness.policy.shell
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class ShellPersistedPolicy(
-    val policies: List<ShellPolicy>
-)
