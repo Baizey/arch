8e6e26e24e46a4fec8848e288a3aa4e1a13fae14
Lasse Spindler Mørk <lassemoerk.mrk@gmail.com>
2026-04-29 18:43:28 +0200

    P

--- agent/src/main/kotlin/org/baizey/harness/policy/git/GitAccessType.kt
+++ agent/src/main/kotlin/org/baizey/harness/policy/git/GitAccessType.kt
@@ -3,9 +3,11 @@ package org.baizey.harness.policy.git
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class GitAccessType(val isModifying: Boolean) {
-    READ(isModifying = false),
-    MODIFY_LOCAL(isModifying = true),
-    PUSH(isModifying = true),
-    PULL(isModifying = true)
+enum class GitAccessType {
+    READ,
+    MODIFY_LOCAL,
+    PUSH,
+    PULL;
+
+    val isModifyingFile get() = this != READ
 }
--- agent/src/main/kotlin/org/baizey/harness/policy/path/FsAccessType.kt
+++ agent/src/main/kotlin/org/baizey/harness/policy/path/FsAccessType.kt
@@ -3,13 +3,12 @@ package org.baizey.harness.policy.path
 import kotlinx.serialization.Serializable
 
 @Serializable
-enum class FsAccessType(val isModifyingFile: Boolean) {
-    DELETE(isModifyingFile = true),
-    WRITE(isModifyingFile = true),
-    EDIT(isModifyingFile = true),
-    EXECUTE(isModifyingFile = true),
-    READ(isModifyingFile = false);
-
+enum class FsAccessType {
+    DELETE,
+    WRITE,
+    EDIT,
+    EXECUTE,
+    READ;
 
     val isModifying get() = this != READ
 }
